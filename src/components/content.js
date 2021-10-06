aa4789e21dbdb008d9512cd15a1e12c847443c56
Yassine Jdair <61687723+YassineJdair@users.noreply.github.com>
2021-10-06 11:32:19 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,12 @@
+//combines two operations
 import React from 'react';
 
 export class Content extends React.Component {
 
     render() {
+         //returns some text and local time
         return (
+              //div is used in HTML to make divisions of content in the web page
             <div>
                 <h1>Hello World!</h1>
                 <h2>It is {new Date().toLocaleTimeString()}.</h2>
