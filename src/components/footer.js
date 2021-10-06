4b12e9b1c05efeb689ff85a8d40ccea4e77cdbf3
Yassine Jdair <61687723+YassineJdair@users.noreply.github.com>
2021-10-06 11:33:06 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,12 @@
+//combines two operations
 import React from 'react';
 
 export class Footer extends React.Component{
 
     render(){
+          //returns some text
         return(
+             //div is used in HTML to make divisions of content in the web page
             <div> 
                 <h1>This is the Footer Component</h1>
             </div>
