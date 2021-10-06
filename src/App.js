4f85f4996c2b73730b52cd3713da821d777e93a5
YASSINE JDAIR - STUDENT <G00377375@gmit.ie>
2021-10-06 11:27:51 +0100

    Lab3 - MyReactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+//combines two operations
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//app class that inherits from components
+
+class App extends Component {
+  //responsible for describing the view to be rendered to the browser window.
+  render() {
+    return (
+      //allows changing browser url
+      <Router>
+
+        {/* //div is used in HTML to make divisions of content in the web page */}
+        <div className="App">
+
+          {/* //navigation bar to navigate to different pages */}
+          <Navbar bg="dark" variant="dark">
+
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+          <br />
+
+           {/* //switch statment evaluates an expression */}
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Header} exact />
+            <Route path='/read' component={Footer} exact />
+          </Switch>
+
+
+        </div>
+      </Router>
+    );
+  }
 }
 
+//export statement is used when creating JavaScript modules to export objects, functions, variables from the module
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+//combines two operations
+import React from 'react';
+
+export class Header extends React.Component{
+
+    render(){
+        //returns some text
+        return(
+            //div is used in HTML to make divisions of content in the web page
+            <div> 
+                <h1>This is the Header Component</h1>
+            </div>
+        );
+    }
+
+}
