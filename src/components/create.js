8f343439b5dbc0afa404c360ddfce053cbd1990a
Yassine Jdair <61687723+YassineJdair@users.noreply.github.com>
2021-10-13 10:30:47 +0100

    Update create.js

--- src/components/create.js
+++ src/components/create.js
@@ -6,7 +6,7 @@ export class Create extends React.Component{
     render(){
         //returns some text
         return(
-            //div is used in HTML to make divisions of content in the web page
+            //div is used in HTML to make divisions of content in the web page //jsx code
             <div> 
                 <h3>Hello from create</h3>
             </div>
