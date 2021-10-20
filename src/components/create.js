cc91954caba8fb5d16bf839a06dbedbeb9c9e972
Yassine Jdair <61687723+YassineJdair@users.noreply.github.com>
2021-10-20 10:52:21 +0100

    Update create.js
    added comments

--- src/components/create.js
+++ src/components/create.js
@@ -5,37 +5,45 @@ export class Create extends React.Component {
 
     constructor() {
         super();
-
+        
+        //provides the scripts data to be executed whenever the submit event is occurred
         this.onSubmit = this.onSubmit.bind(this);
         this.onChangeTitle = this.onChangeTitle.bind(this);
         this.onChangeYear = this.onChangeYear.bind(this);
         this.onChangePoster = this.onChangePoster.bind(this);
-
+        
+        // used by React to represent information about the components current situation
         this.state = {
             Title: '',
             Year: '',
             Poster: ''
         }
     }
-
+    
+    //onchange attributes fires the moment the value of the element is changed
     onChangeTitle(e) {
+        //setState updates value of state based on new user input()
         this.setState({
             Title: e.target.value
         });
     }
 
+    //onchange attributes fires the moment the value of the element is changed
     onChangeYear(e) {
+        //setState updates value of state based on new user input()
         this.setState({
             Year: e.target.value
         });
     }
 
+    //onchange attributes fires the moment the value of the element is changed
     onChangePoster(e) {
+        //setState updates value of state based on new user input()
         this.setState({
             Poster: e.target.value
         });
     }
-
+    //onchange attributes fires the moment the value of the element is changed
     onSubmit(e) {
         e.preventDefault();
         alert("Movie: " + this.state.Title + " " +
@@ -43,7 +51,8 @@ export class Create extends React.Component {
               "Poster: " + this.state.Poster
             );
     }
-
+    
+    //turns website code into the interactive pages users see
     render() {
         //returns some text
         return (
@@ -52,17 +61,21 @@ export class Create extends React.Component {
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Add Movie Title:</label>
+                        //input maintains its own state and gets updated based on user input can only be updated by using setState()
                         <input type='text'
                             className='form-control'
                             value={this.state.Title}
+                            //onchange attributes fires the moment the value of the element is changed
                             onChange={this.onChangeTitle}></input>
 
                     </div>
                     <div className="form,-group">
-                        <labe>Add Movie Year:</labe>
+                        <label>Add Movie Year:</label>
+                        //input maintains its own state and gets updated based on user input can only be updated by using setState()
                         <input type='text'
                             className='form-control'
                             value={this.state.Year}
+                            //onchange attributes fires the moment the value of the element is changed
                             onChange={this.onChangeYear}></input>
                     </div>
 
@@ -71,13 +84,13 @@ export class Create extends React.Component {
                         <textarea type='text'
                         className='form-control'
                         value={this.state.Poster}
+                        //onchange attributes fires the moment the value of the element is changed
                         onChange={this.onChangePoster}>
                         </textarea>
                     </div>
 
-
-
                     <div className="form-group">
+                        //input maintains its own state and gets updated based on user input can only be updated by using setState()
                         <input type='submit'
                             value='Add Movie'
                             className='btn btn-primary'></input>
